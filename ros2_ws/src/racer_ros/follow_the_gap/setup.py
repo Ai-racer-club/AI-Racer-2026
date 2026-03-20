a08ee2dbd7ef80aadcd874c4ad073b266926f6d4
Jonathan Hernandez-Morales <jjonathan.jjhernandez@gmail.com>
2026-03-20 12:39:59 -0400

    follow_the_gap implementation def. needs work on it

--- ros2_ws/src/racer_ros/follow_the_gap/setup.py
+++ ros2_ws/src/racer_ros/follow_the_gap/setup.py
@@ -1,8 +1,7 @@
 from setuptools import find_packages, setup
-import os
 from glob import glob
-
-package_name = 'wall_follow'
+import os
+package_name = 'follow_the_gap'
 
 setup(
     name=package_name,
@@ -12,15 +11,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include launch files
         (os.path.join('share', package_name, 'launch'),
             glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include config files
         (os.path.join('share', package_name, 'config'),
             glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
-    maintainer_email='maxwell.lokshin@2zick.com',
+    maintainer_email='jjonathan.jjhernandez@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
@@ -30,7 +31,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'wall_follow_node = wall_follow.wall_follow_node:main',
+        'reactive_node = follow_the_gap.reactive_node:main',
         ],
     },
 )
